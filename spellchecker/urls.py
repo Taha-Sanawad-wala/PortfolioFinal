a9604a831006c7f037b6832e4ff704cdf950e625
Taha Sanawad <85823152+Taha-Sanawad-wala@users.noreply.github.com>
2023-09-28 23:41:33 +0530

    Delete spellchecker directory

--- spellchecker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from spellchecker.views import index,spell,grammar
-
-urlpatterns = [
-    path('',index),
-    path('spell/',spell),
-    path('grammar/',grammar),
-]
