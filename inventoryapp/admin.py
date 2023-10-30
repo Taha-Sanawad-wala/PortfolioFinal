8a75fb1dd8eb888b39954e623b3833fc76c475ed
Taha Sanawad <85823152+Taha-Sanawad-wala@users.noreply.github.com>
2023-10-30 11:15:50 +0530

    Delete inventoryapp directory

--- inventoryapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from inventoryapp.models import Inventory,InventoryUser
-# Register your models here.
-admin.site.register(Inventory)
-admin.site.register(InventoryUser)
