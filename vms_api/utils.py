de7808a21cdb25442d2eefafc4ae075a92a169b8
Taha Sanawad <85823152+Taha-Sanawad-wala@users.noreply.github.com>
2024-02-05 12:40:20 +0530

    Delete vms_api directory

--- vms_api/utils.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-
-
-def create_token_for_user(username):
-    user=User.objects.get(username=username)
-    token,created=Token.objects.get_or_create(user=user)
-    return token
